07f4881232ba4efd510769a8e0b7d643061bda53
Vanessa O <vaneolipa@gmail.com>
2022-10-04 14:07:53 -0500

    Fix linters errors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import {addBook} from './modules/books.js';
-import {displayList, displayAddNew, displayContact} from './modules/functions.js';
-import {form, navList, navAddNew, navContact, listBooks} from './modules/selectors.js';
+import { addBook } from './modules/UI.js';
+import { displayList, displayAddNew, displayContact } from './modules/functions.js';
+import {
+  form, navList, navAddNew, navContact, listBooks,
+} from './modules/selectors.js';
 
 const eventListeners = () => {
   // add book
@@ -8,46 +10,44 @@ const eventListeners = () => {
 
   // book DOM
   document.addEventListener('DOMContentLoaded', () => {
+    const data = JSON.parse(localStorage.getItem('books')) || [];
 
-    let data = JSON.parse(localStorage.getItem('books')) || [];
-
-    for( let i = 0; i < data.length; i += 1){
+    for (let i = 0; i < data.length; i += 1) {
       const bookContainer = document.createElement('div');
-        bookContainer.classList.add('book-wrap');
-        listBooks.appendChild(bookContainer);
-  
-        const wrap = document.createElement('div');
-        wrap.classList.add('wrap');
-        bookContainer.appendChild(wrap);
-  
-        const tittleBook = document.createElement('p');
-        tittleBook.classList.add('tittle-book');
-        tittleBook.innerText = `"${data[i].tittle}"`;
-        wrap.appendChild(tittleBook);
-  
-        const authorBook = document.createElement('p');
-        authorBook.classList.add('author-book');
-        authorBook.innerText = `by ${data[i].author}`;
-        wrap.appendChild(authorBook);
-  
-        const removeBtn = document.createElement('button');
-        removeBtn.classList.add('btn-remove');
-        removeBtn.innerText = 'Remove';
-        bookContainer.appendChild(removeBtn);
+      bookContainer.classList.add('book-wrap');
+      listBooks.appendChild(bookContainer);
 
-        removeBtn.addEventListener('click', (parameter) =>{
-          if (parameter.target.classList.value === 'btn-remove') {
-            listBooks.removeChild(parameter.target.parentElement);
-          }
-        });
-    }     
-   
+      const wrap = document.createElement('div');
+      wrap.classList.add('wrap');
+      bookContainer.appendChild(wrap);
+
+      const tittleBook = document.createElement('p');
+      tittleBook.classList.add('tittle-book');
+      tittleBook.innerText = `"${data[i].tittle}"`;
+      wrap.appendChild(tittleBook);
+
+      const authorBook = document.createElement('p');
+      authorBook.classList.add('author-book');
+      authorBook.innerText = `by ${data[i].author}`;
+      wrap.appendChild(authorBook);
+
+      const removeBtn = document.createElement('button');
+      removeBtn.classList.add('btn-remove');
+      removeBtn.innerText = 'Remove';
+      bookContainer.appendChild(removeBtn);
+
+      removeBtn.addEventListener('click', (parameter) => {
+        if (parameter.target.classList.value === 'btn-remove') {
+          listBooks.removeChild(parameter.target.parentElement);
+        }
+      });
+    }
   });
 
-  //display sections
+  // display sections
   navList.addEventListener('click', displayList);
   navAddNew.addEventListener('click', displayAddNew);
   navContact.addEventListener('click', displayContact);
-}
+};
 
 eventListeners();
--- modules/functions.js
+++ modules/functions.js
@@ -1,4 +1,6 @@
-import {listBooks, list, addNew, contact} from './selectors.js';
+import {
+  listBooks, list, addNew, contact,
+} from './selectors.js';
 import { DateTime } from './luxon.js';
 
 // Clean HTML
@@ -6,33 +8,32 @@ export const cleanHTML = () => {
   while (listBooks.firstChild) {
     listBooks.removeChild(listBooks.firstChild);
   }
-}
+};
 
-//Display date
+// Display date
 export const displayDate = () => {
   const date = document.querySelector('.date-text');
   setInterval(() => {
     date.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
   }, 0);
-}
+};
 displayDate();
 
-//Display sections
+// Display sections
 export const displayList = () => {
   list.style.display = 'flex';
   addNew.style.display = 'none';
   contact.style.display = 'none';
-}
+};
 
 export const displayAddNew = () => {
   list.style.display = 'none';
   addNew.style.display = 'flex';
   contact.style.display = 'none';
-}
+};
 
 export const displayContact = () => {
   list.style.display = 'none';
   addNew.style.display = 'none';
   contact.style.display = 'flex';
-}
-
+};
